bd7f5e7b4593ccdb2ac521c349d8f64a145167fd
LaughingCoffinLawl <electro600@live.it>
2023-09-21 20:20:52 +0200

    rps-ui exercise completed

--- Javascript/rockpaperscissor/rps.js
+++ Javascript/rockpaperscissor/rps.js
@@ -1,61 +1,71 @@
 let playerScore = 0;
 let computerScore = 0;
 
-while (playerScore < 5 || computerScore < 5) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    playRound(playerChoice, computerChoice);
-    winLoss(playerScore, computerScore);
-    setTimeout(() => {}, 2000);
-}
+const buttons = document.querySelectorAll('input');
 
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Please, choose rock, paper or scissor: ");
-    let playerChoiceNum;
-    if (playerChoice == "scissor") {
-        playerChoiceNum = 2;
-    } else if (playerChoice == "paper") {
-        playerChoiceNum = 1;
-    } else {
-        playerChoiceNum = 0;
-    }
-
-    return playerChoiceNum;
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection) {
+    let computerSelection = getComputerChoice();
     let win_array = [[0, 2, 1], 
                      [1, 0, 2], 
                      [2, 1, 0]];
 
     let result = win_array[playerSelection][computerSelection];
-    console.log("result: " + result);
 
     if (result == 0) {
         console.log("Draw! The player selected: " 
         + playerSelection 
         + ", the computer selected " 
         + computerSelection);
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
     } else if (result == 1) {
         console.log("You won! The player selected: " 
         + playerSelection 
         + ", the computer selected " 
         + computerSelection);
         playerScore++;
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+        
     } else if (result == 2) {
         console.log("You lost! The player selected: " 
         + playerSelection 
         + ", the computer selected " 
         + computerSelection);
         computerScore++;
+
+        if (computerScore == 5) {
+            result += '<br><br>Computer won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
+
+    document.getElementById('result').innerHTML = result;
+    return;
+
 }
 
-function winLoss(playerScore, computerScore) {
+buttons.forEach(button => {button.addEventListener('click', function() {
+        playRound(button.value)
+    })
+})
+
+/* function winLoss(playerScore, computerScore) {
 
     if (playerScore == 5) {
         console.log("You won!")
@@ -68,4 +78,4 @@ function winLoss(playerScore, computerScore) {
         playerScore = 0;
         computerScore = 0;
     }
-}
+} */
